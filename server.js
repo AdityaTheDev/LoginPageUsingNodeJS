635ed3060e99f74c92c7a728b495aa898fe43749
Aditya <84168117+AdityaTheDev@users.noreply.github.com>
2022-03-04 15:15:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -73,4 +73,6 @@ app.delete('/logout',(req,res)=>{
     req.logOut()
     res.redirect('/login')
 })
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
